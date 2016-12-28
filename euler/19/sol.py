e966c2667dcf536f3222e3c154726edc666da992
Raoul Rodriguez <raouljrodriguez@gmail.com>
2016-12-28 17:55:24 -0500

    Solved through number 19.

---
+++ euler/19/sol.py
@@ -0,0 +1,68 @@
+"""
+Solution to Euler Problem number 19.
+
+For humor, we use Conway's doomsday algorithm:
+https://www.timeanddate.com/date/doomsday-weekday.html
+
+This is opposed to the datetime module's strftime method: 
+import datetime
+count = 0
+for y in range(1901, 2001):
+    for m in range(1, 13):
+        if datetime.date(y, m, 1).weekday() == 6:
+            count += 1
+print count #171
+"""
+
+days = {
+    "Sunday": 0,    "Monday": 1,
+    "Tuesday": 2,   "Wednesday": 3,
+    "Thursday": 4,  "Friday": 5,
+    "Saturday": 6
+}
+
+first_of_month_to_closest_doomsday = {
+    3: 6,   4: 3,   5: 8,   6: 5,
+    7: 10,  8: 7,   9: 4,   10: 9,
+    11: 6,  12: 11
+}
+
+def doomsday(year):
+    step_1 = int(str(year)[-2:]) / 12
+    step_2 = int(str(year)[-2:]) % 12
+    step_3 = step_2 / 4
+    if year < 2000:
+        step_4 = days['Wednesday']  # for 1900 - 1999
+    else:
+        step_4 = days['Tuesday']    # for 2000 - 2099
+    return sum([step_1, step_2, step_3, step_4]) % 7
+
+def isLeapYear(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    return False
+
+def day_of_week(month, day, year):
+    dday = doomsday(year)
+    if month == 1:
+        offset = 31 + 27 + isLeapYear(year)
+    elif month == 2:
+        offset = 27 + isLeapYear(year)
+    else:
+        offset = first_of_month_to_closest_doomsday[month]
+    return (dday - offset + day - 1) % 7
+
+
+def counts_months_starting_on(day_name):
+    count = 0
+    for y in range(1901, 2000 + 1):
+        for m in range(1, 12 + 1):
+            if days[day_name] == day_of_week(m, 1, y):
+                count += 1
+    return count
+
+print counts_months_starting_on("Sunday")
---
+++ euler/67/sol.py
@@ -0,0 +1,40 @@
+"""
+Solution to Euler Problem number 67.
+We use a DAG with negative edge weights.
+We leave our code unchanged from the solution 
+for Problem 18.
+"""
+from helpers import open_local_file
+import networkx as nx
+
+FILE = open_local_file('data.txt').read()
+G = nx.DiGraph()
+
+# Set up the pyramid data
+FILE = FILE.split('\n')
+for i in range(0,len(FILE)):
+    FILE[i] = FILE[i].split()
+
+children = [1]
+for l, row in enumerate(FILE):
+    node_num = max(children) + 1
+    
+    for x in row:
+        if (l != len(FILE) - 1):
+            parent = children.pop(0)
+
+            G.add_edge(parent, node_num, weight=-int(x))
+            if node_num not in children:
+                children.append(node_num)
+
+            node_num += 1
+
+            G.add_edge(parent, node_num, weight=-int(x))
+            if node_num not in children:
+                children.append(node_num)
+
+for child, w in zip(children, FILE[-1]):
+    G.add_edge(child, 0, weight=-int(w))
+
+pred, dist = nx.bellman_ford(G, 1)
+print - dist[0]
