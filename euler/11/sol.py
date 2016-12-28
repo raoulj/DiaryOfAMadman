b16915249eae3d911d0785e3b68aa13f6ed3fe04
Raoul Rodriguez <raouljrodriguez@gmail.com>
2016-12-28 01:38:36 -0500

    Adds solution to Problem 11.

--- euler/11/sol.py
+++ euler/11/sol.py
@@ -4,35 +4,38 @@ Solution to Project Euler, Problem 11.
 
 import helpers
 
-def update_champ(current, champ):
-    if current > champ:
-        return current
-    return champ
-
 FILE = helpers.open_local_file('data.txt')
-DATA = [line.split() for line in FILE]
-SIZE = 4
+DATA = [[int(x) for x in row.split()] for row in FILE]
+del row, helpers
+GROUP_SIZE = 4
+ROWCOL_LEN = len(DATA)
 
 champ = 0
-for i, r in enumerate(DATA):
-    for j, x in enumerate(range(0, len(r))):
-        #diagonals to the left
-        if j >= SIZE and i <= len(r) - SIZE:
+for i in range(0, ROWCOL_LEN):
+    for j in range(0, ROWCOL_LEN - GROUP_SIZE):
+        #diagonals
+        if i >= GROUP_SIZE:
+            # to the left
             current = 1
-            for offset in range(0,SIZE):
-                current = DATA[i - offset][j - offset] * current
-        champ = update_champ(champ, current)
+            temp_array = [DATA[j - offset][i - offset] for offset in range(0, GROUP_SIZE)]
+            temp = reduce(lambda x, y: x * y, temp_array, 1)
+            champ = max(champ, temp)
 
-        #diagonals to the right
-        if j <= len(r) - SIZE and len(r) - i >= SIZE:
+            #to the right
             current = 1
-            for offset in range(0,SIZE):
-                current = DATA[i + offset][j + offset] * current
-        champ = update_champ(champ, current)
+            temp_array = [DATA[j - offset][i + offset - GROUP_SIZE] for offset in range(0, GROUP_SIZE)]
+            temp = reduce(lambda x, y: x * y, temp_array, 1)
+            champ = max(champ, temp)
 
         #horizontal
+        current = 1
+        temp_array = [DATA[i][j + offset] for offset in range(0, GROUP_SIZE)]
+        temp = reduce(lambda x, y: x * y, temp_array, 1)
+        champ = max(champ, current)
+
         #verical
-        test = reduce(lambda x, y: x * y, r[i:i+4],1)
-        if test > h_max:
-            hmax = test
-print hmax    
+        current = 1
+        temp_array = [DATA[j + offset][i] for offset in range(0, GROUP_SIZE)]
+        temp = reduce(lambda x, y: x * y, temp_array, 1)
+        champ = max(champ, current)
+print champ
