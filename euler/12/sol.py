4e5305441b9dd8ab9449282b81a650ce7569b331
Raoul Rodriguez <raouljrodriguez@gmail.com>
2016-12-28 02:24:08 -0500

    Solution to Problem 12.

---
+++ euler/12/sol.py
@@ -0,0 +1,25 @@
+"""
+Solution to Euler Problem number 12.
+"""
+import time
+from sympy.ntheory import factorint
+
+def triangle_numbers():
+    """
+    Generator for the "triangle numbers": 1, 3, 6, 10...
+    Each term in the sequence is a partial sum of the natural numbers
+    1 = 1, 1 + 2 = 3, 1 + 2 + 3 = 6, 1 + 2 + 3 + 4 = 10, 1 + 2 + ...
+    """
+    sum = 0
+    i = 1
+    while True:
+        sum += i
+        yield sum
+        i += 1
+
+start = time.time()
+for n in triangle_numbers():
+    div_count = reduce(lambda x, y: x * (y + 1), factorint(n).values(), 1)
+    if div_count > 500:
+        print n
+        break
